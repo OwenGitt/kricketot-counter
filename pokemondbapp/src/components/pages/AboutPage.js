3daca05444bbd861be5c884a5075901830e324e4
Owen_Gittins <99190962+OwenGitt@users.noreply.github.com>
2023-06-07 11:09:05 +0100

    Edited json file
    Edited hunts.json to properly hold all my shiny hunting data to be used and displayed on the about page.

--- pokemondbapp/src/components/pages/AboutPage.js
+++ pokemondbapp/src/components/pages/AboutPage.js
@@ -1,7 +1,17 @@
+import React from "react";
+import "../styleSheets/AboutStyles.css";
+import hunts from "../jsonData/hunts.json";
+
 function AboutPage() {
+  const allHunts = hunts["hunts"].map((value, index) => (
+    <section key={value} className="pokemonCardSection">
+      {console.log(value["Encounters"])}
+    </section>
+  ));
+
   return (
-    <div className="counterPageContainer">
-      <div className="cardContainer"></div>
+    <div className="aboutPageContainer">
+      <div className="aboutCardContainer"></div>
     </div>
   );
 }
